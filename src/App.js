70c1ad24190e44a379b47c58e1cc86dec7c4c85b
Riya Nigam <97540322+Riyanigam-1234@users.noreply.github.com>
2022-11-10 17:47:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App-">
+      <header className="App-header">
+      <h1>ABOUT US</h1>
+      <p>She decided water-skiing on a frozen lake wasn't a good idea.</p>
+      <p>
+      Be an independent woman is the best for three reasons. The first, an independent woman has a stronger mental. For example, she is not easy to complain. And then, she does not easily give up in a difficult situation. Moreover, she will face problems with courage. And also, she wants to work hard to achieve the goal. The second, an independent woman can do anything without depending on others. Because of that, she will has extensive experience. Generally, she has strong principle that is not easily influenced by others. Besides, she does not to bothersome people around her. Even more, she can motivate herself when others do not care. The last, an independent woman more appreciated by man. When a woman has the financial independence, she is not easily to be underestimated by man. Furthermore, an independent woman will be more trusted by man to do something rather than a spoiled woman. Basically, man needs a woman who can ease the problem, so he needs an independent woman. For that three reasons, we should be an independent woman because it will make us as a strong woman and more appreciated by others.
+      </p>
+      <p>
+      Firstly, mental quality of an independent woman is more strong. Usually, she prefers to act rather than complain. So that, when she is in trouble, she will not easy to despair___________.
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
